0a0671cbcd95fc7566be02112d800a2b3bdd480a
Tyler Kranzen <tok4416@rit.edu>
2024-11-07 09:41:28 -0500

    adding init

---
+++ src/test/java/edu/rit/swen253/test/WikipediaWebTest.java
@@ -0,0 +1,19 @@
+package edu.rit.swen253.test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class WikipediaWebTest extends AbstractWebTest{
+    
+
+
+
+
+}
