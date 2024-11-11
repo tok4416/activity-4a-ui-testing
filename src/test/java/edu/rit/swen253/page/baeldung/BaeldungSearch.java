41365cf0126973241e5d417d7948c3d46b549e36
Duncan French <duncanlfrench@gmail.com>
2024-11-10 19:59:15 -0500

    Added test and functionality for logging search results.

--- src/test/java/edu/rit/swen253/page/baeldung/BaeldungSearch.java
+++ src/test/java/edu/rit/swen253/page/baeldung/BaeldungSearch.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
 
 import edu.rit.swen253.page.AbstractPage;
@@ -11,15 +15,15 @@ import edu.rit.swen253.utils.DomElement;
 
 public class BaeldungSearch extends AbstractPage {
     
-    // private static final By SEARCH_NAVIGATION_FINDER = By.className("menu-search");
+    // finders to extract various elements from key pages
     private static final By SEARCH_NAVIGATION_FINDER = By.id("menu-item-40489");
     private static final By POPUP_CLOSE_FINDER = By.className("qc-usp-close-icon");
     private static final By SEARCH_TEXT_FIELD_FINDER = By.id("search");
     private static final By SEARCH_ENTRY_BUTTON_FINDER = By.className("btn-search-icon");
+    private static final By SEARCH_RESULT_CONTAINER_FINDER = By.className("archive-columns");
 
+    // storing the search icon that will be initialized in the constructor to allow other methods to use it for navigation
     private DomElement searchIcon;
-    private DomElement searchTextBar;
-    private DomElement searchSubmissionButton;
 
     public BaeldungSearch() {
         super();
@@ -37,13 +41,14 @@ public class BaeldungSearch extends AbstractPage {
     }
 
     public void goToSearchPage() {
-        searchIcon.click();
+        searchIcon.click(); // clicking the icon opens the search view (it does not change the current URL)
     }
 
     public void enterSearchText(String text) {
         try {
-            searchTextBar = findOnPage(SEARCH_TEXT_FIELD_FINDER);
-            searchTextBar.click();
+            DomElement searchTextBar = findOnPage(SEARCH_TEXT_FIELD_FINDER);
+            // we click the textbar and THEN begin entering text since Firefox does not allow text entry when the element isn't selected yet
+            searchTextBar.click(); 
             searchTextBar.enterText(text);
         } catch (TimeoutException e) {
             fail("Search textbar element not found");
@@ -52,7 +57,8 @@ public class BaeldungSearch extends AbstractPage {
 
     public void submitSearch() {
         try {
-            searchSubmissionButton = findOnPage(SEARCH_ENTRY_BUTTON_FINDER);
+            // finding the search button & clicking it to submit our search terms
+            DomElement searchSubmissionButton = findOnPage(SEARCH_ENTRY_BUTTON_FINDER);
             searchSubmissionButton.click();
         } catch (TimeoutException e) {
             fail("Search submission button element not found");
--- src/test/java/edu/rit/swen253/test/baeldung/BaeldungSearchTest.java
+++ src/test/java/edu/rit/swen253/test/baeldung/BaeldungSearchTest.java
@@ -1,29 +1,56 @@
 package edu.rit.swen253.test.baeldung;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.bidi.log.LogLevel;
 
 import edu.rit.swen253.page.baeldung.BaeldungSearch;
+import edu.rit.swen253.page.baeldung.BaeldungSearchResults;
 import edu.rit.swen253.test.AbstractWebTest;
+import edu.rit.swen253.test.sample.RitExploreStudiesInComputingTest;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BaeldungSearchTest extends AbstractWebTest {
-    private BaeldungSearch home;
+    private static final Logger logger = Logger.getLogger(RitExploreStudiesInComputingTest.class.getName());
+    private BaeldungSearch homePage;
+    private BaeldungSearchResults resultsPage;
 
-
+    /*
+     * First, navigate to the search page from the home page
+     */
     @Test
     @Order(1)
     public void goToSearchPage() {
-        home = navigateToPage("https://baeldung.com", BaeldungSearch::new);
-        home.goToSearchPage();
+        homePage = navigateToPage("https://baeldung.com", BaeldungSearch::new);
+        homePage.goToSearchPage();
     }
 
+    /*
+     * Second, enter a search term and submit, allowing the site to fetch results
+     */
     @Test
     @Order(2)
     public void enterSearch() {
-        home.enterSearchText("java");
-        home.submitSearch();
+        homePage.enterSearchText("page object model");
+        homePage.submitSearch();
+        // constructing the page to view search results
+        resultsPage = navigateToPage("https://baeldung.com/?s=page+object+model", BaeldungSearchResults::new);
+    }
+
+    /*
+     * Third, log all the search results on the page
+     */
+    @Test
+    @Order(3)
+    public void logSearchResults() {
+        Map<String, String> resultTitleToURL = resultsPage.getSearchResults(); // apparently this is stale? yes, actually bc we navigated off the page - need to do smth about this
+        for (String title : resultTitleToURL.keySet()) {
+            logger.info("\nTitle: " + title + " URL: " + resultTitleToURL.get(title) + "\n");
+        }
     }
 }
