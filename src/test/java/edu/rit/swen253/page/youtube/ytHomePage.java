42f3d900bccf79cd4742aa9e193ef5634c9518e7
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-11-11 09:48:43 -0500

    finishing up pages?

--- src/test/java/edu/rit/swen253/page/youtube/ytHomePage.java
+++ src/test/java/edu/rit/swen253/page/youtube/ytHomePage.java
@@ -2,34 +2,24 @@ package edu.rit.swen253.page.youtube;
 
 import edu.rit.swen253.page.AbstractPage;
 import edu.rit.swen253.utils.DomElement;
-import edu.rit.swen253.utils.HtmlUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 
-import java.util.List;
-import java.util.logging.Logger;
-
 import static org.junit.jupiter.api.Assertions.fail;
 
-
 /**
- * A Page Object for the RIT Home page.
+ * A Page Object for the Youtube Home page.
  *
  * @author <a href='mailto:mot7506@g.rit.edu'>Miles Tallia</a>
  */
 public class ytHomePage extends AbstractPage {
-
-  /**
-   * A finder to extract the main body content.
-   * WARN: this is fragile code
-   */
+  
   private static final By SEARCH_CONTAINER_FINDER =  By.id("search");
 
   private DomElement searchBox;
 
   public ytHomePage() {
     super();
-    // validate basic page structure
     try {
       searchBox = findOnPage(SEARCH_CONTAINER_FINDER);
     } catch (TimeoutException e) {
